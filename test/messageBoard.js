db1539b82c220b81214e45f83fe4873c07dcc98b
kohei sano <nlee9337@gmail.com>
2025-06-12 22:37:12 +0900

    test: add unit test for message storage in postMessage function

--- test/messageBoard.js
+++ test/messageBoard.js
@@ -12,4 +12,26 @@ describe("MessageBoard", function () {
     // コントラクトアドレスが存在するか確認
     expect(board.target).to.properAddress;
   });
+
+  it("should store a message when postMessage is called", async function () {
+    const MessageBoard = await hre.ethers.getContractFactory("MessageBoard");
+    const board = await MessageBoard.deploy();
+    await board.waitForDeployment();
+
+    const [sender] = await hre.ethers.getSigners();
+    const testText = "Hello, blockchain!";
+
+    // post a message
+    await board.connect(sender).postMessage(testText);
+
+    const count = await board.getMessageCount();
+    expect(count).to.equal(1);
+
+    const messages = await board.getLatestMessages(1);
+    const message = messages[0];
+
+    expect(message.sender).to.equal(sender.address);
+    expect(message.text).to.equal(testText);
+    expect(message.timestamp).to.be.a("bigint");
+  })
 });
