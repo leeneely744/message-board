529f44cb40f4a2084a2d8419a3b1339bb6b25a61
kohei sano <nlee9337@gmail.com>
2025-06-15 17:32:18 +0900

    feat: enhance deployment script to write contract address to JSON file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import pkg from "hardhat";
 const { ethers } = pkg;
 
@@ -8,6 +9,14 @@ async function main() {
     await messageBoard.waitForDeployment();
 
     console.log("MessageBoard deployed to:", messageBoard.target);
+
+    // Make sure to place the message-board-ui project in the same directory as this project.
+    fs.writeFileSync(
+        "../message-board-ui/src/contract-address.json",
+        JSON.stringify({ address: messageBoard.target }, null, 2)
+    );
+
+    console.log("MessageBoard.json created successfully");
 }
 
 main().then(() => process.exit(0)).catch((error) => {
