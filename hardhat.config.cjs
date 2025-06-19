ac7e39814367303d8749a605212ed6030c5a1c86
kohei sano <nlee9337@gmail.com>
2025-06-19 22:43:19 +0900

    fix: update faucet task logging format and correct account parameter in balance task in Hardhat configuration

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -3,22 +3,24 @@ const { task } = require("hardhat/config");
 const { parseEther } = require("ethers");
 require("@nomicfoundation/hardhat-toolbox");
 
+// npx hardhat faucet --network localhost --to ${MetaMask_account_address}
 task("faucet", "Send 10 ETH to an address")
   .addParam("to", "Recipient address")
   .setAction(async ({ to }, hre) => {
     const [sender] = await hre.ethers.getSigners();
-    console.log(`💧  Sending 10 ETH from ${sender.address} → ${to}`);
+
+    console.log(`💧 Sending 10 ETH from ${sender.address} → ${to}`);
 
     const tx = await sender.sendTransaction({
       to,
       value: parseEther("10"),
     });
-    await tx.wait();
 
-    console.log(`✅  TX mined: ${tx.hash}`);
+    await tx.wait();
+    console.log(`✅ TX mined: ${tx.hash}`);
   });
 
-// npx hardhat balance --account ${contract_address}
+// npx hardhat balance --account ${account_address}
 task("balance", "Prints an account's balance")
   .addParam("account", "The account's address")
   .setAction(async (taskArgs) => {
